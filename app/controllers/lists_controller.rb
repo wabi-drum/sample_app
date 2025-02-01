15badb0da424173523677fc0eeb0529ff75a3619
EC2 Default User <ec2-user@ip-172-31-42-165.ap-northeast-1.compute.internal>
2025-02-01 12:00:04 +0000

    アプリケーションを完成させよう8章終わり

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,6 +9,15 @@ class ListsController < ApplicationController
     redirect_to list_path(list.id)
   end
 
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
+  end
+
   def index
     @lists = List.all 
   end
